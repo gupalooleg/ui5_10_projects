b19438ac1eb8bd531f9bb00a2684bc8d524bd94b
Aleh Hupalo <Aleh_Hupalo@epam.com>
2022-01-10 17:12:06 +0300

    feat: add ability to use url instead route

--- uimodule/webapp/controller/Tiles.controller.js
+++ uimodule/webapp/controller/Tiles.controller.js
@@ -1,9 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/m/library",
     "com/ah/uimodule/model/models"
 ],
 
-    function (Controller, models) {
+    function (Controller,
+    library,
+	models) {
         "use strict";
 
         return Controller.extend("com.ah.uimodule.controller.Tiles", {
@@ -13,7 +16,14 @@ sap.ui.define([
             },
 
             onTilePress: function(sRoute){
-                this.getOwnerComponent().getRouter().navTo(sRoute);
+                var oTiles = this.getView().getModel("tiles").getObject("/");
+                var oRouteTile = oTiles.find((oTile) => oTile.route === sRoute);
+
+                if (oRouteTile.url){
+                    library.URLHelper.redirect(oRouteTile.url);
+                }else{
+                    this.getOwnerComponent().getRouter().navTo(sRoute);
+                }
             }
         });
     });
